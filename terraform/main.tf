d2d0021aded8ae22f5df91f4d2c881a8a0d5b51e
UjwalNagrikar <ujjwalnagrikar@gmail.com>
2026-09-11 21:08:53 +0530

    Networking trfic rule chnage

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,26 +23,10 @@ resource "aws_security_group" "vllm" {
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    description = "SSH from operator IP"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.ssh_cidr]
-  }
-
-  ingress {
-    description = "HTTP for ingress"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTPS for ingress"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    description = "Allow all inbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
